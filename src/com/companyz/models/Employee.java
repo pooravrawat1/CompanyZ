6b3e319649aef14bbf5854f741996b97b162425a
Poorav Rawat <160499644+pooravrawat1@users.noreply.github.com>
2025-12-11 20:41:14 +0000

    JobTitleID is int now

--- src/com/companyz/models/Employee.java
+++ src/com/companyz/models/Employee.java
@@ -64,8 +64,8 @@ public class Employee {
     public String getJobTitle() { return jobTitle; }
     public void setJobTitle(String jobTitle) { this.jobTitle = jobTitle; }
 
-    public String getJobTitleID() { return jobTitle; }
-    public void setJobTitleIDID(int jobTitleID) { this.jobTitleID = jobTitleID; }
+    public int getJobTitleID() { return jobTitleID; }
+    public void setJobTitleID(int jobTitleID) { this.jobTitleID = jobTitleID; }
 
     public String getDivision() { return division; }
     public void setDivision(String division) { this.division = division; }
